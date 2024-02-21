8f128be136404d31d5b4fd5894133aa61b6dfc76
Temsaah <Seif.Temsaah@gmail.com>
2024-02-21 18:00:22 +0200

    Updated mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "primary-tomato": "hsl(4, 100%, 67%)",
         "neutral-dark-slate-grey": "hsl(234, 29%, 20%)",
         "neutral-charcoal-grey": "hsl(235, 18%, 26%)",
-        grey: "hsl(231, 7%, 60%)",
+        "neutral-grey": "hsl(231, 7%, 60%)",
       },
       fontFamily: {
         rob: ["Roboto"],
